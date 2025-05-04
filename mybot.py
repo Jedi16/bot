45000b1eff180ffb692674327a8454c2e335f676
Jedi16 <63871064+Jedi16@users.noreply.github.com>
2025-05-04 20:17:34 +0200

    Update mybot.py

--- mybot.py
+++ mybot.py
@@ -12,7 +12,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
+print("running nowww")
 # Path to Edge WebDriver
 EDGE_DRIVER_PATH = r"C:\Users\20232036\OneDrive - TU Eindhoven\Desktop\webdriver\msedgedriver.exe"
 
